cb570da00e9be475e583ff5c69f5532e5658d26c
Juneu <corma642@naver.com>
2025-10-31 12:05:10 +0900

    NEW : 플러그인 및 모듈 추가 & Fixed Pools 설정 추가
    추가 플러그인
     - Gameplay Abilities
     - Motion Warping
     - Skeletal Merging
    
    추가 모듈
     - "AIModule"
     - "StateTreeModule"
     - "GameplayStateTreeModule"
     - "UMG"
     - "Slate"
     - "GameplayTags"
     - "GameplayAbilities"
     - "GameplayTasks"
     - "AnimGraphRuntime"
     - "NavigationSystem"
     - "Niagara"
     - "MovieScene"
     - "MovieSceneTracks"
     - "LevelSequence"
    
    프로젝트 공개 폴더 경로 설정
     - 프로젝트 경로의 헤더 추가 시, 프로젝트 폴더 경로도 같이 읽도록 함.

--- Source/Arua/Arua.Build.cs
+++ Source/Arua/Arua.Build.cs
@@ -4,20 +4,41 @@ using UnrealBuildTool;
 
 public class Arua : ModuleRules
 {
-	public Arua(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+    public Arua(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // 프로젝트 경로 설정
+        PublicIncludePaths.Add("Arua");
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "AIModule",
+            "StateTreeModule",
+            "GameplayStateTreeModule",
+            "UMG",
+            "Slate",
+            "GameplayTags",
+            "GameplayAbilities",
+            "GameplayTasks",
+            "AnimGraphRuntime",
+            "NavigationSystem",
+            "Niagara",
+            "MovieScene",
+            "MovieSceneTracks",
+            "LevelSequence",
+        });
+
+
+        PrivateDependencyModuleNames.AddRange(new string[] { });
+
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
