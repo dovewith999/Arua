7cd739f49b0af0ca0bfcd14505990be702c9c9e2
Limhs <limhs991013@gmail.com>
2025-10-31 14:32:01 +0900

    NEW : ViewBase, ViewModel Base 클래스 생성
    - MVVM 기초 클래스 생성

---
+++ Source/Arua/UI/View/ViewBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ViewBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARUA_API UViewBase : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Arua/UI/ViewModel/ViewModelBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ViewModelBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARUA_API UViewModelBase : public UObject
+{
+	GENERATED_BODY()
+	
+};
