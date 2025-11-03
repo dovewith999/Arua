7ad7991ece22f9ab0ad29aa9111cd6d24030a652
Juneu <corma642@naver.com>
2025-11-03 10:28:25 +0900

    NEW : 에디터에 Blueprints 폴더 추가 & GameMode 클래스 폴더 추가 & GameInstance 클래스 폴더 추가
    에디터에 공용 Blueprints 폴더 생성
    
    GameMode 클래스 폴더 추가
    
    GameInstance 클래스 폴더 추가

---
+++ Source/Arua/GameModes/AruaGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AruaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARUA_API AAruaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
