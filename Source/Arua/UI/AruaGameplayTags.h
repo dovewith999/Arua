b2bdbccb64b4be0a07ea6fcc0cf3c8e9709eb204
dovewith999 <Limhs991013@gmail.com>
2025-11-03 18:20:02 +0900

    NEW : Common UI 세팅

---
+++ Source/Arua/UI/AruaGameplayTags.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace AruaGameplayTags
+{
+	ARUA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Arua_WidgetStack_Model);
+	ARUA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Arua_WidgetStack_GameMenu);
+	ARUA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Arua_WidgetStack_GameHud);
+	ARUA_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Arua_WidgetStack_Frontend);
+}
+
--- Source/Arua/UI/Test/CommonUIPlayerController.cpp
+++ Source/Arua/UI/Test/CommonUIPlayerController.cpp
@@ -2,4 +2,18 @@
 
 
 #include "UI/Test/CommonUIPlayerController.h"
+#include "Kismet/GameplayStatics.h"
+#include "Camera/CameraActor.h"
 
+void ACommonUIPlayerController::OnPossess(APawn* aPawn)
+{
+	Super::OnPossess(aPawn);
+	
+	TArray<AActor*> FoundCameras;
+	UGameplayStatics::GetAllActorsOfClassWithTag(this, ACameraActor::StaticClass(), FName("Default"), FoundCameras);
+
+	if (!FoundCameras.IsEmpty())
+	{
+		SetViewTarget(FoundCameras[0]);
+	}
+}
