cbcb3377fc11df8174bc8f73ebd263dab4c7dd0b
Juneu <corma642@naver.com>
2025-11-04 11:35:14 +0900

    CHG : 퀘스트 컴포넌트 구현
    퀘스트 컴포넌트 구현
     - 플레이어에게 붙이는 UQuestComponent, 퀘스트 목록 보관, 퀘스트 수주/완료 콜백 제공
     - 퀘스트 활성화 구조체
     - - 활성화(진행중)중인 퀘스트의 ID
     - - 활성화(진행중)중인 퀘스트의 진행도 (퀘스트 목표 수치)
     - - 활성화(진행중)중인 퀘스트의 완료 여부
     - 퀘스트 수락 델리게이트
     - 퀘스트 완료 델리게이트
     - 활성화한 퀘스트 관리 배열
     - 퀘스트 수락 함수
     - - 새 퀘스트 활성화 구조체를 생성하여 활성화한 퀘스트 관리 배열에 추가
     - - 퀘스트 수락 델리게이트 브로드캐스트
     - 퀘스트 진행도 업데이트 함수 (퀘스트 완료 확인 / 이후 구현)
     - - 퀘스트 관리 배열 순회
     - - 동일한 퀘스트 ID이고, 퀘스트가 완료되지 않은 경우 퀘스트의 진행도 업데이트
     - - 퀘스트 진행도와 퀘스트 목표 수치 비교 후 완료 처리 구현 [이후 구현]

--- Source/Arua/Components/Quest/QuestComponent.cpp
+++ Source/Arua/Components/Quest/QuestComponent.cpp
@@ -3,32 +3,71 @@
 
 #include "Components/Quest/QuestComponent.h"
 
-// Sets default values for this component's properties
 UQuestComponent::UQuestComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+	PrimaryComponentTick.bCanEverTick = false;
 }
 
+bool UQuestComponent::AcceptQuest(const FQuestData& QuestData)
+{
+	// 이미 활성화중인 퀘스트인 경우, 퀘스트 수락 실패
+	if (IsQuestActive(QuestData.QuestID))
+	{
+		return false;
+	}
 
-// Called when the game starts
+	// 새 퀘스트 활성화 구조체 생성
+	FActiveQuest NewQuest;
+	NewQuest.QuestID = QuestData.QuestID;	// 퀘스트 ID 초기화
+	NewQuest.Progress = 0;					// 퀘스트 진행도 0으로 초기화
+	NewQuest.bCompleted = false;			// 퀘스트 완료 여부 false로 초기화
+
+	// 활성화한 퀘스트 관리 배열에 추가
+	ActiveQuests.Add(NewQuest);
+
+	// 퀘스트 수락 델리게이트 브로드캐스트
+	OnQuestAccepted.Broadcast(QuestData.QuestID);
+
+	return true;
+}
+
+bool UQuestComponent::UpdateQuestProgress(const FName& QuestID, int32 Amount)
+{
+	// 퀘스트 관리 배열 순회
+	for (auto& Quest : ActiveQuests)
+	{
+		// 동일한 퀘스트 ID이고, 퀘스트가 완료되지 않은 경우
+		if (Quest.QuestID == QuestID && !Quest.bCompleted)
+		{
+			// 퀘스트의 진행도 업데이트
+			Quest.Progress += Amount;
+
+			// TODO 김준형: 퀘스트 진행도와 퀘스트 목표 수치 비교 후 완료 처리 구현
+			//if (Quest.Progress >= )
+
+			return true;
+		}
+	}
+
+	return false;
+}
+
 void UQuestComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
 }
 
-
-// Called every frame
-void UQuestComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+bool UQuestComponent::IsQuestActive(const FName& QuestID) const
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	// 활성화한 퀘스트 관리 배열을 순회
+	for (const auto& Quest : ActiveQuests)
+	{
+		// 동일한 QuestID를 활성화 중인 경우 true 반환
+		if (Quest.QuestID == QuestID)
+		{
+			return true;
+		}
+	}
 
-	// ...
+	return false;
 }
-
--- Source/Arua/Components/Quest/QuestComponent.h
+++ Source/Arua/Components/Quest/QuestComponent.h
@@ -4,25 +4,72 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "DataTables/QuestData.h"
 #include "QuestComponent.generated.h"
 
+// 퀘스트 활성화 구조체
+USTRUCT(BlueprintType)
+struct FActiveQuest
+{
+	GENERATED_BODY()
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+	// 퀘스트 ID
+	UPROPERTY(BlueprintReadOnly)
+	FName QuestID;
+
+	// 퀘스트 진행도 (퀘스트 목표 수치)
+	UPROPERTY(BlueprintReadOnly)
+	int32 Progress = 0;
+
+	// 퀘스트 완료 여부
+	UPROPERTY(BlueprintReadOnly)
+	bool bCompleted = false;
+};
+
+// 퀘스트 수락 델리게이트
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnQuestAccepted, const FName&, QuestID);
+
+// 퀘스트 완료 델리게이트
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnQuestCompleted, const FName&, QuestID);
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class ARUA_API UQuestComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
-	// Sets default values for this component's properties
+public:
 	UQuestComponent();
 
+	// 퀘스트 수락 함수
+	UFUNCTION(BlueprintCallable)
+	bool AcceptQuest(const FQuestData& QuestData);
+
+	// 퀘스트 진행도 업데이트 함수 (퀘스트 완료 확인)
+	UFUNCTION(BlueprintCallable)
+	bool UpdateQuestProgress(const FName& QuestID, int32 Amount = 1);
+
+	// 활성화한 퀘스트 관리 배열 Getter 함수
+	UFUNCTION(BlueprintCallable)
+	const TArray<FActiveQuest>& GetActiveQuests() const { return ActiveQuests; }
+
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	// 퀘스트 활성화(진행 중) 여부
+	bool IsQuestActive(const FName& QuestID) const;
 
-		
+public:
+	// 퀘스트 수락 델리게이트 변수
+	UPROPERTY(BlueprintAssignable)
+	FOnQuestAccepted OnQuestAccepted;
+
+	// 퀘스트 완료 델리게이트 변수
+	UPROPERTY(BlueprintAssignable)
+	FOnQuestCompleted OnQuestCompleted;
+
+protected:
+	// 활성화한 퀘스트 관리 배열
+	UPROPERTY()
+	TArray<FActiveQuest> ActiveQuests;
+
 };
