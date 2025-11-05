ca03bea6e652ed9e4ddab0ebec9949137c93cd2e
Limhs <limhs991013@gmail.com>
2025-11-05 14:59:00 +0900

    NEW : TargetLock 위젯 및 위젯 컴포넌트 생성

---
+++ Source/Arua/UI/WidgetComponent/ARWidgetComponent.cpp
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/WidgetComponent/ARWidgetComponent.h"
+#include "UI/View/ViewBase.h"
+
+void UARWidgetComponent::InitWidget()
+{
+	Super::InitWidget();
+
+	// 상위에서 InitWidget 함수가 호출된 이후에는 위젯 인스턴스가 생성됐다는 것을 보장할 수 있다.
+	if (UViewBase* UserWidget = Cast<UViewBase>(GetWidget()))
+	{
+		UserWidget->SetOwningActor(GetOwner());
+	}
+}
