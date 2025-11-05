15bfaa441bb5dab8fa7962e5dfd74f42e8cc9c8a
jsub1379 <145996022+jsub1379@users.noreply.github.com>
2025-11-05 16:10:13 +0900

    New : 플레이어 방향으로 공격하는 간단한 BT 적용
    - 플레이어 탐지 -> 1초 대기 -> 공격 애니메이션 적용 -> 1초 대기 순서로 BT 적용
    - ABP 에 간단한 State Machine 작동 가능하도록 수정

---
+++ Source/Arua/Animation/ARBossAnimInstance.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ARBossAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARUA_API UARBossAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	//UARBossAnimInstance();
+
+
+};
