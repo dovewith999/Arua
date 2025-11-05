9354c041169dcb0cc46ac8c78f174cd4dd14b919
Limhs <limhs991013@gmail.com>
2025-11-05 14:56:37 +0900

    NEW : MonsterBase 클래스 및 TestBoss BP 생성
    - 몬스터들이 공통적으로 상속 받을 Base 클래스 생성
    - TargetLock 테스트용 BP_TestBoss 생성

---
+++ Source/Arua/Enemy/ARMonsterBase.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy/ARMonsterBase.h"
+#include "UI/WidgetComponent/ARWidgetComponent.h"
+
+AARMonsterBase::AARMonsterBase()
+{
+	// Widget Component
+	TargetLock = CreateDefaultSubobject<UARWidgetComponent>(TEXT("Widget"));
+	TargetLock->SetupAttachment(GetMesh());
+
+	// 위치 조정
+	TargetLock->SetRelativeLocation(FVector(0.f, 0.f, 200.f));
+
+	// 어떤 Widget BP를 사용해 그릴지
+	static ConstructorHelpers::FClassFinder<UUserWidget> TargetLockViewRef(TEXT("/Game/Personal/LIM_H_S/UI/WBP_TargetLock.WBP_TargetLock_C"));
+	if (TargetLockViewRef.Succeeded())
+	{
+		// 컴포넌트에서 사용할 위젯 클래스 설정
+		TargetLock->SetWidgetClass(TargetLockViewRef.Class);
+
+		// 위젯을 그릴 공간 지정
+		TargetLock->SetWidgetSpace(EWidgetSpace::Screen);
+
+		// 그릴 크기 지정
+		TargetLock->SetDrawSize(FVector2D(100.f, 100.f));
+		TargetLock->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	}
+}
+
+void AARMonsterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/Arua/Enemy/ARMonsterBase.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ARCharacterBase.h"
+#include "ARMonsterBase.generated.h"
+
+/**
+ * 몬스터들의 부모가 될 클래스
+ * 몬스터들이 공통으로 가지는 기능을 정의한다.
+ * 작성자 : 임희섭
+ * 작성일 : 25/11/05
+ */
+UCLASS()
+class ARUA_API AARMonsterBase : public AARCharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	AARMonsterBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	// 타겟으로 지정됐을 때 띄울 3D 위젯 - 25/11/05 임희섭
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "UI|Target", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UARWidgetComponent> TargetLock;
+};
