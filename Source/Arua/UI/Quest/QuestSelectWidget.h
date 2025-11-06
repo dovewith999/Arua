e5c42be80fab48bbdbeb60cbd7118b398754aecf
Juneu <corma642@naver.com>
2025-11-06 15:00:23 +0900

    NEW : 퀘스트 선택 위젯에서 퀘스트 수락/반납(완료)에 맞는 리스트 관리 및 보상 수령 버튼 활성화 분기 처리
    NEW : 퀘스트 선택 위젯에서 퀘스트 수락/반납(완료)에 맞는 리스트 관리 및 보상 수령 버튼 활성화 분기 처리
     -  퀘스트 수락 위젯의 수락 모드에 따라 퀘스트 수락/퀘스트 반납 분기 처리
     - 수락 가능한 퀘스트 목록/보상 수령 가능한 퀘스트 목록으로 나눠 관리 (진행 상태에 따른 중복 퀘스트 생성 방지)
     - 플레이어의 퀘스트 컴포넌트를 통해 각 퀘스트의 진행 상태를 확인 후 상태에 맞게 버튼 위젯 관리
     - - [수락 전] : 이 퀘스트를 수락할 수 있으므로 수락 가능한 퀘스트 버튼 생성
     - - [수락 및 진행] : 이 퀘스트를 이미 수락했으므로 퀘스트 버튼 생성 안 함
     - - [반납] : 이 퀘스트를 완료했지만 보상은 수령하지 않았으므로, [완료] 텍스트 추가 후 보상 수령 가능한 퀘스트 버튼 생성
     - 퀘스트 수락 및 반납 요청 브로드캐스트

--- Source/Arua/UI/Quest/QuestSelectWidget.h
+++ Source/Arua/UI/Quest/QuestSelectWidget.h
@@ -9,6 +9,9 @@
 // 퀘스트 수락 델리게이트 (UI 정리 및 퀘스트 액터의 퀘스트 정리)
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnQuestAccepted, FName, QuestID);
 
+// 퀘스트 보상 수령 델리게이트
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnQuestTurnInRequested, FName, QuestID);
+
 UCLASS()
 class ARUA_API UQuestSelectWidget : public UUserWidget
 {
@@ -16,7 +19,7 @@ class ARUA_API UQuestSelectWidget : public UUserWidget
 
 public:
 	// 퀘스트 리스트 초기화 함수
-	void InitQuestList(const TArray<FName>& QuestIDs, class UDataTable* InQuestDataTable);
+	void InitQuestList(APawn* InInteractor, const TArray<FName>& QuestIDs, class UDataTable* InQuestDataTable);
 
 protected:
 	// 퀘스트 버튼 위젯 선택(클릭) 콜백 함수
@@ -27,11 +30,20 @@ protected:
 	UFUNCTION()
 	void HandleOnQuestAccepted(FName QuestID);
 
+	// 퀘스트 보상 수령 콜백 함수
+	UFUNCTION()
+	void HandleOnQuestTurnIn(FName QuestID);
+
 public:
 	// 퀘스트 수락 델리게이트 객체
 	UPROPERTY(BlueprintAssignable, Category = "Quest")
 	FOnQuestAccepted OnQuestAccepted;
 
+	// 퀘스트 보상 수령 델리게이트 객체
+	UPROPERTY(BlueprintAssignable, Category = "Quest")
+	FOnQuestTurnInRequested OnQuestTurnInRequested;
+
+protected:
 	// 퀘스트 버튼 위젯들이 들어갈 컨테이너
 	UPROPERTY(BlueprintReadOnly, Category = "UI", meta = (BindWidget))
 	TObjectPtr<class UVerticalBox> QuestButtonContainer;
@@ -40,11 +52,11 @@ public:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "UI")
 	TSubclassOf<class UQuestButtonWidget> QuestButtonWidgetClass;
 
-	// 퀘스트 수락 위젯이 들어갈 컨테이너
+	// 퀘스트 수락/완료 위젯이 들어갈 컨테이너
 	UPROPERTY(BlueprintReadOnly, Category = "UI", meta = (BindWidget))
 	TObjectPtr<class USizeBox> QuestAcceptContainer;
 
-	// 퀘스트 수락 위젯 클래스
+	// 퀘스트 수락/완료 위젯 클래스
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "UI")
 	TSubclassOf<class UQuestAcceptWidget> QuestAcceptWidgetClass;
 
@@ -57,7 +69,11 @@ private:
 	UPROPERTY()
 	class UDataTable* QuestDataTable;
 
-	// 선택 가능한 퀘스트 목록
+	// 수락 가능한 퀘스트 목록
 	UPROPERTY()
-	TMap<FName, class UQuestButtonWidget*> ButtonMap;
+	TMap<FName, class UQuestButtonWidget*> AcceptButtons;
+
+	// 보상 수령 가능한 퀘스트 목록
+	UPROPERTY()
+	TMap<FName, class UQuestButtonWidget*> TurnInButtons;
 };
