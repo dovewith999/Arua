f00977e84e63da2326efb72f571719e552ce3887
Juneu <corma642@naver.com>
2025-11-06 14:49:17 +0900

    NEW : 퀘스트 컴포넌트에서 퀘스트의 진행 상태 추가 & 퀘스트 완료 시, 실제 보상 지급 로직 및 퀘스트 목록 정리(진행 가능 퀘스트/완료된 퀘스트)
    NEW : 퀘스트 컴포넌트에서 퀘스트의 진행 상태 추가 & 퀘스트 완료 시, 실제 보상 지급 로직 및 퀘스트 목록 정리(진행 가능 퀘스트/완료된 퀘스트)
     - 퀘스트 [수락 전] -> [수락] -> [진행] -> [반납] -> [완료] 구조
     - 퀘스트 아이템(ex. 적 처치라면 퀘스트 아이템은 '적', 허브 수집이라면 퀘스트 아이템은 '허브')
     - 퀘스트 아이템과 플레이어간의 상호작용 시, 퀘스트 진행도 갱신 함수를 통해 타입/타깃 검사 후 진행도 갱신
     - 퀘스트 목표 수치에 만족 시, 반납 대기 상태로 전환 (보상은 아직 X)
     - 퀘스트 HUD / 퀘스트 저널에서 [완료 가능]으로 표시
     - 퀘스트를 받은 퀘스트 NPC에게 가서 퀘스트 반납(완료) 시, 보상 지급 및 퀘스트의 완료 처리 (진행 가능 퀘스트 목록에서 완료된 퀘스트 목록으로 이동)
     - 보상 지급 로직 수행 (이후 인벤토리/골드 연동)

--- Source/Arua/Components/Quest/QuestComponent.h
+++ Source/Arua/Components/Quest/QuestComponent.h
@@ -7,6 +7,16 @@
 #include "DataTables/QuestData.h"
 #include "QuestComponent.generated.h"
 
+// 퀘스트 진행 상태 열거형
+UENUM(BlueprintType)
+enum class EQuestStatus : uint8
+{
+	None,			// 수락하지 않은 상태
+	InProgress,     // 목표 미달성
+	ReadyToTurnIn,  // 목표 달성, NPC에게 반납 필요
+	TurnedIn        // 반납 완료(보상 지급됨)
+};
+
 // 퀘스트 진행 구조체
 USTRUCT(BlueprintType)
 struct FActiveQuest
@@ -21,16 +31,22 @@ struct FActiveQuest
 	UPROPERTY(BlueprintReadOnly)
 	int32 CurrentCount = 0;
 
-	// 퀘스트 완료 여부
+	// 퀘스트 진행 상태
 	UPROPERTY(BlueprintReadOnly)
-	bool bCompleted = false;
+	EQuestStatus Status = EQuestStatus::None;
 };
 
-// 퀘스트 진행도 갱신 델리게이트
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnQuestProgress, FName, QuestID, int32, CurrentCount);
+// 퀘스트 수락 델리게이트
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnQuestAcceptedSig, const FQuestData&, Quest);
 
-// 퀘스트 완료 델리게이트
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnQuestCompleted, FName, QuestID);
+// 퀘스트 진행도 갱신 델리게이트
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnQuestProgressSig, FName, QuestID, int32, CurrentCount);
+
+// 퀘스트 목표 달성 및 반납 필요 처리 델리게이트
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnQuestReadyToTurnInSig, FName, QuestID);
+
+// 퀘스트 반납 완료 델리게이트
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnQuestTurnedInSig, FName, QuestID);
 
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class ARUA_API UQuestComponent : public UActorComponent
@@ -48,22 +64,37 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void AddProgressByEvent(EQuestType Type, FName Target, int32 Amount = 1);
 
-	// 직접 완료 체크 함수(옵션)
+	// NPC에게 반납(보상 지급 시점)
+	UFUNCTION(BlueprintCallable)
+	bool TurnInQuest(FName QuestID);
+
+	// 자가 퀘스트 완료 함수(옵션)
 	UFUNCTION(BlueprintCallable)
 	void TryCompleteQuest(FName QuestID);
+
+	// 퀘스트 진행 상태 반환 함수
+	const EQuestStatus GetQuestStatusFromQuestID(const FName QuestID) const;
 
 protected:
 	// 보상 지급 함수
 	void GrantReward(const FQuestData& Quest);
 
 public:
+	// 퀘스트 수락 델리게이트 객체
+	UPROPERTY(BlueprintAssignable)
+	FOnQuestAcceptedSig OnQuestAcceptedSig;
+
 	// 퀘스트 진행도 갱신 델리게이트 객체
 	UPROPERTY(BlueprintAssignable)
-	FOnQuestProgress OnQuestProgress;
+	FOnQuestProgressSig OnQuestProgressSig;
 
-	// 퀘스트 완료 델리게이트 객체
+	// 퀘스트 목표 달성 및 반납 필요 처리 델리게이트 객체
 	UPROPERTY(BlueprintAssignable)
-	FOnQuestCompleted OnQuestCompleted;
+	FOnQuestReadyToTurnInSig OnQuestReadyToTurnInSig;
+
+	// 퀘스트 반납 완료 델리게이트 객체
+	UPROPERTY(BlueprintAssignable)
+	FOnQuestTurnedInSig OnQuestTurnedInSig;
 
 	// 진행중인 퀘스트 목록 (퀘스트 ID, 퀘스트 데이터)
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Quest")
