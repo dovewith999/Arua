15d66bd1aa7c6f7bf0118e6f6dbed0287a1e8b2c
Juneu <corma642@naver.com>
2025-11-10 10:25:15 +0900

    NEW : DataAseet 구현용 C++ 폴더 추가 & 아이템 고정 데이터 에셋 추가

---
+++ Source/Arua/DataAssets/Item/DA_ItemDefinition.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DA_ItemDefinition.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARUA_API UDA_ItemDefinition : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};
