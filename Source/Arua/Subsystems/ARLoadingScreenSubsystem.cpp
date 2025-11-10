7796dbb19d086c3570b542d5de73e26b9b8fa877
Limhs <limhs991013@gmail.com>
2025-11-10 10:03:23 +0900

    NEW : LoadingScreen Sub System 생성

---
+++ Source/Arua/Subsystems/ARLoadingScreenSubsystem.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ARLoadingScreenSubsystem.h"
+
+bool UARLoadingScreenSubsystem::ShouldCreateSubsystem(UObject* Outer) const
+{
+	if (!CastChecked<UGameInstance>(Outer)->IsDedicatedServerInstance())
+	{
+		TArray<UClass*> FoundClasses;
+		GetDerivedClasses(GetClass(), FoundClasses);
+
+		return FoundClasses.IsEmpty();
+	}
+
+	return false;
+}
+
+void UARLoadingScreenSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	FCoreUObjectDelegates::PreLoadMapWithContext.AddUObject(this, &ThisClass::OnMapPreLoaded);
+	FCoreUObjectDelegates::PostLoadMapWithWorld.AddUObject(this, &ThisClass::OnMapPostLoaded);
+}
+
+void UARLoadingScreenSubsystem::Deinitialize()
+{
+	FCoreUObjectDelegates::PreLoadMapWithContext.RemoveAll(this);
+	FCoreUObjectDelegates::PostLoadMapWithWorld.RemoveAll(this);
+}
+
+void UARLoadingScreenSubsystem::OnMapPreLoaded(const FWorldContext& WorldContext, const FString& MapName)
+{
+	UE_LOG(LogTemp, Log, TEXT("On Map Pre Loaded"));
+}
+
+void UARLoadingScreenSubsystem::OnMapPostLoaded(UWorld* LoadedWorld)
+{
+	UE_LOG(LogTemp, Log, TEXT("On Map Post Loaded"));
+}
---
+++ Source/Arua/Subsystems/ARLoadingScreenSubsystem.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "ARLoadingScreenSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARUA_API UARLoadingScreenSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	// Begin USubSystem Interface
+	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+
+	/** Implement this for initialization of instances of the system */
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+	/** Implement this for deinitialization of instances of the system */
+	virtual void Deinitialize() override;
+	// End USubSystem Interface
+
+private:
+	// 맵이 로드되기 이전에 실행될 함수
+	UFUNCTION()
+	void OnMapPreLoaded(const FWorldContext& WorldContext, const FString& MapName); 
+	
+	// 맵이 로드된 이후에 실행된 함수
+	UFUNCTION()
+	void OnMapPostLoaded(UWorld* LoadedWorld);
+};
