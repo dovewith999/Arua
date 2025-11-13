c28f0050c54e7bfb52d708961b813f3af58e9ee4
Limhs <limhs991013@gmail.com>
2025-11-13 15:30:51 +0900

    NEW : ARTA_TraceBase 클래스 및 ARTA_TarceArea 클래스 생성
    - 기존 ARTA_Trace가 TraceBase를 상속받도록 구조 변경

---
+++ Source/Arua/Character/GA/TA/ARTA_TraceBase.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/GA/TA/ARTA_TraceBase.h"
+
+void AARTA_TraceBase::StartTargeting(UGameplayAbility* Ability)
+{
+	Super::StartTargeting(Ability);
+}
+
+void AARTA_TraceBase::ConfirmTargetingAndContinue()
+{
+	Super::ConfirmTargeting();
+}
+
+
+FGameplayAbilityTargetDataHandle AARTA_TraceBase:: MakeTargetData() const
+{
+	return FGameplayAbilityTargetDataHandle();
+}
