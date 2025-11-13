8d37d6be39137f2bb5060d6bb8ffaed9cae9db1e
Juneu <corma642@naver.com>
2025-11-13 09:03:33 +0900

    NEW : 아이템 픽업 액터 구현
    NEW : 아이템 픽업 액터 구현
     - 월드에 배치되는 아이템 픽업 액터
     - 플레이어가 상호작용하면 인벤토리에 아이템을 추가하고 자기 자신을 제거함
     - 이후에 퀘스트 NPC와 상호작용 우선순위 구현해야 할 듯

--- Source/Arua/Actors/Items/ItemPickupActor.cpp
+++ Source/Arua/Actors/Items/ItemPickupActor.cpp
@@ -16,6 +16,23 @@ AItemPickupActor::AItemPickupActor()
 	// 루트 컴포넌트 설정
 	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
 
+	// 아이템 메시 컴포넌트 초기화
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMesh"));
+	ItemMesh->SetupAttachment(RootComponent);
+
+	// 충돌 컴포넌트 초기화
+	InteractionCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("InteractionCollision"));
+	InteractionCollision->SetupAttachment(RootComponent);
+	InteractionCollision->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	InteractionCollision->SetCollisionObjectType(ECC_WorldDynamic);
+	InteractionCollision->SetCollisionResponseToAllChannels(ECR_Ignore);
+	InteractionCollision->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+	// 충돌 오버랩 함수 바인딩
+	InteractionCollision->OnComponentBeginOverlap.AddDynamic(this, &AItemPickupActor::OnBeginOverlap);
+
+	// 기본 수량 초기화
+	Quantity = 1;
 }
 
 void AItemPickupActor::BeginPlay()
@@ -25,10 +42,29 @@ void AItemPickupActor::BeginPlay()
 
 void AItemPickupActor::Interact(APawn* Interactor)
 {
+	if (!Interactor || !ItemDefinition) return;
 
+	// 플레이어의 인벤토리 컴포넌트 가져오기
+	UInventoryComponent* InventoryComp = Interactor->FindComponentByClass<UInventoryComponent>();
+	if (InventoryComp)
+	{
+		// 아이템 추가
+		int32 Added = InventoryComp->AddItem(ItemDefinition, Quantity);
+		if (Added > 0)
+		{
+			// 성공적으로 추가되면 아이템 액터 삭제
+			Destroy();
+		}
+	}
 }
 
 void AItemPickupActor::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	if (APawn* Pawn = Cast<APawn>(OtherActor))
+	{
+		// 플레이어에게 위젯 띄우고 픽업 입력 액션 할당하기?
 
+		// 오버랩 되었을 때 자동으로 인터랙트하지 않고, 플레이어 입력에 의해 호출되도록 하려면 인터랙트 함수를 호출하지 않는다.
+		// 예: 캐릭터가 E 키를 눌렀을 때 주변 아이템을 찾도록 구현 가능.
+	}
 }
--- Source/Arua/Actors/Items/ItemPickupActor.h
+++ Source/Arua/Actors/Items/ItemPickupActor.h
@@ -8,9 +8,8 @@
 
 /*
 * 월드에 배치되는 아이템 픽업 액터
-* 플레이어가 상호작용하면 인벤토리에 아이템을 추가하고 자기 자신을 제거한다.
+* 플레이어가 상호작용하면 인벤토리에 아이템을 추가하고 자기 자신을 제거
 */
-
 UCLASS()
 class ARUA_API AItemPickupActor : public AActor
 {
@@ -22,30 +21,27 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
-	/** 플레이어와 상호작용하여 아이템을 인벤토리에 추가한다.
-	 * @param Interactor 상호작용하는 캐릭터
-	 */
+	// 아이템 상호작용 함수
+	/* 플레이어와 상호작용하여 아이템을 인벤토리에 추가 후, 액터 제거 */
 	UFUNCTION(BlueprintCallable, Category = "ItemActor")
 	void Interact(APawn* Interactor);
 
-	/** 충돌 시작 시 호출되어 플레이어가 들어왔는지 확인한다. */
+	// 충돌 오버랩 함수 (플레이어에게 픽업 메시지 전달을 구현할 경우)
 	UFUNCTION()
-	void OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
-		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
-		bool bFromSweep, const FHitResult& SweepResult);
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 protected:
-	//// 아이템 메시
-	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "ItemActor")
-	//TObjectPtr<class UStaticMeshComponent> ItemMesh;
+	// 아이템 메시
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "ItemActor")
+	TObjectPtr<class UStaticMeshComponent> ItemMesh;
 
-	//// 상호작용 영역
-	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "ItemActor")
-	//TObjectPtr<class UBoxComponent> InteractionVolume;
+	// 아이템 상호작용 콜리전
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "ItemActor")
+	TObjectPtr<class UBoxComponent> InteractionCollision;
 
-	//// 아이템 정의 (아이템 데이터 에셋)
-	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ItemActor")
-	//TObjectPtr<class UItemDefinition> ItemDefinition;
+	// 아이템 정의 (아이템 데이터 에셋)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ItemActor")
+	TObjectPtr<class UDA_ItemDefinition> ItemDefinition;
 
 	// 수량
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ItemActor")
