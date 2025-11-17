7c85d97d215007fb39ddde2c79af1e9b4abf4ee7
Juneu <corma642@naver.com>
2025-11-17 15:17:21 +0900

    NEW : 소모품 아이템 정의 구현

---
+++ Source/Arua/DataAssets/Item/DA_ConsumableDefinition.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DataAssets/Item/DA_ItemDefinition.h"
+#include "DA_ConsumableDefinition.generated.h"
+
+/**
+ * 플레이어 속성과 연동되는 소모품 아이템 정의
+ */
+UCLASS()
+class ARUA_API UDA_ConsumableDefinition : public UDA_ItemDefinition
+{
+	GENERATED_BODY()
+	
+public:
+	// 퀘스트 아이템 사용 효과
+	virtual void ApplyEffect_Implementation(AActor* User) override;
+
+	// 소모품 효과 게임플레이 이펙트
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Consumable")
+	TSubclassOf<class UGameplayEffect> ConsumableGameplayEffect;
+};
