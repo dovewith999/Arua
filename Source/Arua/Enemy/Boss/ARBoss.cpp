d5775b0bb35eba74a9cb2f5ae778b1b8b61c8687
jsub1379 <145996022+jsub1379@users.noreply.github.com>
2025-11-20 12:54:55 +0900

    NaviMesh 크기 조정 완료
    백스텝 패턴이 나와서 범위 밖으로 나가지 않도록 크기 조정

--- Source/Arua/Enemy/Boss/ARBoss.cpp
+++ Source/Arua/Enemy/Boss/ARBoss.cpp
@@ -167,6 +167,7 @@ void AARBoss::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 		DrawDebugSphere(GetWorld(), GetActorLocation(), BossAttackRange, 32, FColor::Red, false, -1.f, 0, 2.f);
+		DrawDebugSphere(GetWorld(), NestLocation, BossNestRange, 32, FColor::Purple, false, -1.f, 0, 2.f);
 
 }
 
