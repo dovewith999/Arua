c32a084ff2031a7d2e816590e4ede3d132b1444d
Juneu <corma642@naver.com>
2025-11-20 11:28:28 +0900

    CNG : 소모품 아이템 정의 변경
    CNG : 소모품 아이템 정의 변경
     - 소모품 아이템이 적용할 게임플레이 이펙트를 복수 적용 가능하도록 구현
     - - ex) 체력 5 회복 및 이동 속도 50 증가...

--- Source/Arua/DataAssets/Item/DA_ConsumableDefinition.h
+++ Source/Arua/DataAssets/Item/DA_ConsumableDefinition.h
@@ -8,26 +8,39 @@
 #include "DA_ConsumableDefinition.generated.h"
 
 /**
+ * 소모품 아이템이 적용할 게임플레이 이펙트 구조체
+ */
+USTRUCT(BlueprintType)
+struct FConsumableGameplayEffect
+{
+	GENERATED_BODY()
+
+	// 적용할 GameplayEffect 클래스
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Consumable")
+	TSubclassOf<class UGameplayEffect> GameplayEffectClass;
+
+	// 적용 수치
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Consumable")
+	float EffectAmount = 10.f;
+
+	// SetByCaller 게임 플레이 태그
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Consumable")
+	FGameplayTag SetByCallerTag;
+};
+
+/**
  * 소모품 아이템 정의 클래스
  */
 UCLASS()
 class ARUA_API UDA_ConsumableDefinition : public UDA_ItemDefinition
 {
 	GENERATED_BODY()
-	
+
 public:
 	// 퀘스트 아이템 사용 효과
 	virtual void ApplyEffect_Implementation(AActor* User) override;
 
-	// 소모품 효과 게임플레이 이펙트
+	// 소모품 효과 게임플레이 이펙트 (복수 적용 가능)
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Consumable")
-	TSubclassOf<class UGameplayEffect> ConsumableGameplayEffect;
-
-	// 체력 회복량
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Consumable")
-	float HealAmount = 50.f;
-
-	// SetByCaller에 사용할 게임플레이 태그
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Consumable")
-	FGameplayTag HealAmountTag;
+	TArray<FConsumableGameplayEffect> ConsumableGameplayEffects;
 };
